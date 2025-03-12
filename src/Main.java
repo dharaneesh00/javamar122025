f6db1d453bb4136a81eadccd1d1bf5a5e2887976
dharaneesh00 <rmdharaneeshh@gmail.com>
2025-03-12 17:51:49 +0530

    3rd

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 class Demo {
     static public void main(String[] args) {
-        System.out.println("my name is jack danile");
-        System.out.println("my designation is best brand");
+        System.out.println("Door No: D089\n" +
+                "Street: St. Louis Street\n" +
+                "City: Springfield\n" +
+                "ZIP Code: 62729");
 
     }
 }
