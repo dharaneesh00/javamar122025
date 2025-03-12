03be6224550bd6118ec7b7c8b88402f848ba50d1
dharaneesh00 <rmdharaneeshh@gmail.com>
2025-03-12 17:42:22 +0530

    2nd
    🥲

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 class Demo {
     static public void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("my name is jack danile");
+        System.out.println("my designation is best brand");
 
     }
 }
