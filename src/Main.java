0cc780caaaa6fa6e28e322ff0c8e1413fc2e4fd4
dharaneesh00 <rmdharaneeshh@gmail.com>
2025-09-30 09:35:18 +0530

    SINGLY LL

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,161 @@
-class Calculator {
+class SLL {
+    Node head;
 
-    public double findAverage(double num1, double num2, double num3) {
-        double average = (num1 + num2 + num3) / 3;
-        return Math.round(average * 100.0) / 100.0;
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
     }
-}
 
+    // Insert at beginning
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
 
+    // Insert at end
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Insert at middle (by position)
+    public void insertAtMiddle(int data, int position) {
+        if (position == 1) {
+            insertAtBeginning(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        for (int i = 0; i < position - 1 && temp != null; i++) {
+            temp = temp.next;
+        }
+        if (temp == null) {
+            System.out.println("Position out of range.");
+            return;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    // Delete at beginning
+    public void deleteAtBeginning() {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+        head = head.next;
+    }
+
+    // Delete at end
+    public void deleteAtEnd() {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+    }
+
+    // Delete at middle (by position)
+    public void deleteAtMiddle(int position) {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+        if (position == 1) {
+            deleteAtBeginning();
+            return;
+        }
+        Node temp = head;
+        for (int i = 1; i < position - 1 && temp.next != null; i++) {
+            temp = temp.next;
+        }
+        if (temp.next == null) {
+            System.out.println("Position out of range.");
+            return;
+        }
+        if(temp.next!=null){
+            temp.next.prev=temp.prev;
+        }
+        temp.next = temp.next.next;
+    }
+
+    // Search
+    public boolean search(int data) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == data) {
+                return true;
+
+                temp = temp.next;
+            }
+            return false;
+        }
+
+        // Display
+        public void display() {
+            if (head == null) {
+                System.out.println("List is empty.");
+                return;
+            }
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+
+    public class Main {
+        public static void main(String[] args) {
+            SLL list = new SLL();
+
+            // Insertions
+            list.insertAtEnd(10);
+            list.insertAtEnd(20);
+            list.insertAtEnd(30);
+            list.display();
+
+            list.insertAtBeginning(5);
+            list.display();
+
+            list.insertAtMiddle(15, 3);
+            list.display();
+
+            // Deletions
+            list.deleteAtBeginning();
+            list.display();
+
+            list.deleteAtEnd();
+            list.display();
+
+            list.deleteAtMiddle(2);
+            list.display();
+
+            // Searching
+            System.out.println("Searching 20: " + list.search(20));
+            System.out.println("Searching 100: " + list.search(100));
+        }
+    }
